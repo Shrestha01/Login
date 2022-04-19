100cf0d7bffcba7d91bc09d1cf461a79d64afa5c
Adarsha Shrestha <adarsha@192-168-1-104.tpgi.com.au>
2022-04-19 15:04:35 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState(0);
+  function apple() {
+    setName(name + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {name}</h1>
+      <button onClick={apple}>Click Me</button>
     </div>
   );
 }
---
+++ src/component/form/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "../form/Login.css";
+import { Table, Button } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [allEntry, setAllEntry] = useState([]);
+  const submitForm = (e) => {
+    e.preventDefault();
+    const newEntry = { email: email, password: password };
+    setAllEntry([...allEntry, newEntry]);
+    console.log(allEntry);
+  };
+  return (
+    <>
+      <h1 className={`primary`}>Login Form</h1>
+
+      <form action="" onSubmit={submitForm}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <Button type="submit">Login</Button>
+      </form>
+      <br />
+      <div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <td>Email ID</td>
+              <td>Password</td>
+            </tr>
+          </thead>
+          <tbody>
+            {allEntry.map((curElement) => {
+              return (
+                <>
+                  <tr>
+                    <td>{curElement.email}</td>
+                    <td>{curElement.password}</td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </>
+  );
+};
+export default Login;
